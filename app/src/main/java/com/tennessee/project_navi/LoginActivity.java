8d629dd50a020f84ebac060fed6c60e445b263ce
S-T Hwang <hst007@naver.com>
2021-05-08 21:33:17 +0900

    11weekProject

--- app/src/main/java/com/tennessee/project_navi/LoginActivity.java
+++ app/src/main/java/com/tennessee/project_navi/LoginActivity.java
@@ -6,10 +6,21 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
 
 import androidx.annotation.Nullable;
 
+import com.kakao.auth.ISessionCallback;
+import com.kakao.auth.Session;
+import com.kakao.network.ErrorResult;
+import com.kakao.usermgmt.UserManagement;
+import com.kakao.usermgmt.callback.MeV2ResponseCallback;
+import com.kakao.usermgmt.response.MeV2Response;
+import com.kakao.util.exception.KakaoException;
+
 public class LoginActivity extends Activity {
+
+    private ISessionCallback mSessionCallback;
 
     EditText ID, Pass;
     Button btnLogin, btnJoin;
@@ -17,6 +28,44 @@ public class LoginActivity extends Activity {
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.login_page);
+
+        mSessionCallback = new ISessionCallback() {
+            @Override
+            public void onSessionOpened() {
+                // 로그인 요청
+                UserManagement.getInstance().me(new MeV2ResponseCallback() {
+                    @Override
+                    public void onFailure(ErrorResult errorResult) {
+                        //로그인 실패
+                        Toast.makeText(LoginActivity.this,"로그인에 실패하였습니다.",Toast.LENGTH_SHORT).show();
+                    }
+
+                    @Override
+                    public void onSessionClosed(ErrorResult errorResult) {
+                        // 세션이 닫힘
+                        Toast.makeText(LoginActivity.this,"세션이 닫혔습니다..다시 시도해주세요",Toast.LENGTH_SHORT).show();
+                    }
+
+                    @Override
+                    public void onSuccess(MeV2Response result) {
+                        // 로그인 성공
+                        Intent intent = new Intent(LoginActivity.this, MainActivity.class);
+                       /* intent.putExtra("name", result.getKakaoAccount().getProfile().getNickname());
+                        intent.putExtra("email",result.getKakaoAccount().getEmail());
+                        intent.putExtra("profileImg",result.getKakaoAccount().getProfile().getProfileImageUrl());*/
+                        startActivity(intent);
+
+                        Toast.makeText(LoginActivity.this,"로그인에 성공하였습니다.",Toast.LENGTH_SHORT).show();
+                    }
+                });
+            }
+
+            @Override
+            public void onSessionOpenFailed(KakaoException exception) {
+                Toast.makeText(LoginActivity.this, "onSessionOpenFailed",Toast.LENGTH_SHORT).show();
+            }
+
+        };
 
         ID = findViewById(R.id.login_ID);
         Pass = findViewById(R.id.login_Pass);
