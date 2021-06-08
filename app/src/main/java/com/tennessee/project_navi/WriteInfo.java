477246c445aad1594755f5e00227d9a8521ef0e5
S-T Hwang <hst007@naver.com>
2021-06-09 07:07:22 +0900

    0609commit

--- app/src/main/java/com/tennessee/project_navi/WriteInfo.java
+++ app/src/main/java/com/tennessee/project_navi/WriteInfo.java
@@ -1,38 +1,52 @@
 package com.tennessee.project_navi;
 
+import java.util.Date;
+
 public class WriteInfo {
     private String title;
     private String Contents;
     private String publisher;
+    private Date date;
 
-    public WriteInfo(String title, String Contents, String publisher){
+    public WriteInfo(String title, String Contents, String publisher, Date date) {
         this.title = title;
         this.Contents = Contents;
         this.publisher = publisher;
+        this.date = date;
 
     }
 
-    public String getTitle(){
+    public String getTitle() {
         return this.title;
     }
-    public void setTitle(String title){
+
+    public void setTitle(String title) {
         this.title = title;
     }
 
-    public String getContents(){
+    public String getContents() {
         return this.Contents;
     }
-    public void setContents(String Contents){
+
+    public void setContents(String Contents) {
         this.Contents = Contents;
     }
 
-    public String getPublisher(){
+    public String getPublisher() {
         return this.publisher;
     }
-    public void setPublisher(String publisher){
+
+    public void setPublisher(String publisher) {
         this.publisher = publisher;
+
+
+        public Date getDate () {
+            return this.date;
+        }
+        public void setDate (Date date){
+            this.date = date;
+        }
+
+
     }
-
-
-
 }
