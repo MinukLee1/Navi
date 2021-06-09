1e5e419475a1a68b72fe8a2272f9371c3eee0c7d
jhsun <wngus2991@gmail.com>
2021-06-09 19:43:36 +0900

    second design

--- app/src/main/java/com/tennessee/project_navi/MainActivity.java
+++ app/src/main/java/com/tennessee/project_navi/MainActivity.java
@@ -21,6 +21,9 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.FrameLayout;
 
+import com.google.android.gms.ads.AdRequest;
+import com.google.android.gms.ads.AdView;
+import com.google.android.gms.ads.MobileAds;
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
 import com.google.android.material.bottomnavigation.BottomNavigationView;
@@ -38,8 +41,7 @@ public class   MainActivity extends AppCompatActivity {
     private Context mContext;
     private Fragment HomeFragment,SearchFragment,FeedFragment,BookmarkFragment,MypageFragment;
     private static final String TAG = "MainActivity";
-
-
+    private AdView mAdView;
 
 
     @Override
@@ -50,6 +52,11 @@ public class   MainActivity extends AppCompatActivity {
         mContext = getApplication();
         getHashKey(mContext);
 
+        //Google AdMob
+        MobileAds.initialize(this, getString(R.string.admob_app_id));
+        mAdView = findViewById(R.id.adView);
+        AdRequest adRequest = new AdRequest.Builder().build();
+        mAdView.loadAd(adRequest);
 
 
 
